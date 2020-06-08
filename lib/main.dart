a7d7a5fda5fa757f5edf576686bb358a921d8bbb
Lduffley <53620669+Lduffley@users.noreply.github.com>
2020-06-07 23:38:22 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
